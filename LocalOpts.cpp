0ecaa7e09b25afb030a389b3a3115e9ab091cab1
AlessandroSchena <alle.schena@gmail.com>
2024-04-04 11:22:52 +0200

    add strenght reduction function

--- LocalOpts.cpp
+++ LocalOpts.cpp
@@ -15,22 +15,32 @@
 #include <llvm/Transforms/Utils/BasicBlockUtils.h>
 
 using namespace llvm;
-//Function to print algebrical identity msg
+//Function to print Algebrical Identity msg
 void print_algebrical_identity(Instruction& i, Value *op, bool AddOrMul){
   if(AddOrMul == true){
     outs() << "ADD ALGEBRICAL IDENTITY\n";
   } else{
     outs() << "MUL ALGEBRICAL IDENTITY\n";
   }
-  outs() << "\tUSES OF " ;
+  outs() << "\tUSES OF: " ;
   i.printAsOperand(outs(), false);
   outs() << " REPLACE WITH: ";
   op->printAsOperand(outs(), false);
   outs() << "\n";
 }
 
+//Function to print Strength Reductio msg
+void print_strength_reduction(Instruction& i, Instruction* is){
+  outs() << "STRENGHT REDUCTION\n";
+  outs() << "\tUSES OF: " << i << " -> ";
+  i.printAsOperand(outs(), false);
+  outs() << " REPLACE WITH: " << *is << " -> ";
+  is->printAsOperand(outs(), false);
+  outs() << "\n";
+}
 
-//Function implements algebrical identity
+
+//Function implements Algebrical Identity
 void AlgebricIdentity(BasicBlock &B){
   Value *op1, *op2;
   for(Instruction &inst : B){
@@ -72,6 +82,116 @@ void AlgebricIdentity(BasicBlock &B){
   }
 }
 
+//Function implements Strenght Reduction
+void StrengthReduction(BasicBlock &B){
+  outs() << "ENTERING IN STRENGTH REDUCTION FUNCTION\n";
+  Value *op1, *op2;
+  for(Instruction &inst : B){
+    op1 = inst.getOperand(0);
+    op2 = inst.getOperand(1);
+    if(inst.getOpcode() == Instruction::Mul){
+      if(ConstantInt *C = dyn_cast<ConstantInt>(op1)){
+        if(C->getValue().isPowerOf2()){
+          auto numShift = C->getValue().logBase2();
+          Constant *C1 = ConstantInt::get(C->getType(), numShift);
+          Instruction *shiftInst = BinaryOperator::CreateShl(op2, C1);
+
+          shiftInst->insertAfter(&inst);
+          inst.replaceAllUsesWith(shiftInst);
+
+          outs() << "MUL ";
+          print_strength_reduction(inst, shiftInst);
+        }
+        else{
+          if(C->getValue().ugt(1)){
+            APInt n = C->getValue()+1;
+            if(n.isPowerOf2()){
+              auto numShift = n.logBase2();
+              Constant *C1 = ConstantInt::get(C->getType(), numShift);
+              Instruction *shiftInst = BinaryOperator::CreateShl(op2, C1);
+              Instruction *difInst = BinaryOperator::CreateSub(shiftInst, op2);
+
+              shiftInst->insertAfter(&inst);
+              difInst->insertAfter(shiftInst);
+              inst.replaceAllUsesWith(difInst);
+
+              outs() << "MUL +1" << "\n";
+              print_strength_reduction(inst, difInst);
+              outs() << "\t\t" << n << "\n";
+            }
+          }
+        }
+      }
+      if(ConstantInt *C = dyn_cast<ConstantInt>(op2)){
+        if(C->getValue().isPowerOf2()){
+          auto numShift = C->getValue().logBase2();
+          Constant *C1 = ConstantInt::get(C->getType(), numShift);
+          Instruction *shiftInst = BinaryOperator::CreateShl(op1, C1);
+
+          shiftInst->insertAfter(&inst);
+          inst.replaceAllUsesWith(shiftInst);
+
+          outs() << "MUL ";
+          print_strength_reduction(inst, shiftInst);
+          outs() << "\tconst: " << *C1 << "\n";
+        }
+        else{
+          if(C->getValue().ugt(1)){
+            APInt n = C->getValue()+1;
+            if(n.isPowerOf2()){
+              auto numShift = n.logBase2();
+              Constant *C1 = ConstantInt::get(C->getType(), numShift);
+              Instruction *shiftInst = BinaryOperator::CreateShl(op1, C1);
+              Instruction *difInst = BinaryOperator::CreateSub(shiftInst, op1);
+
+              shiftInst->insertAfter(&inst);
+              difInst->insertAfter(shiftInst);
+              inst.replaceAllUsesWith(difInst);
+              
+              outs() << "\tMUL +1" << "\n";
+              print_strength_reduction(inst, difInst);
+              outs() << "\t\t" << n << "\n";
+            }
+          }
+        }
+      }
+    }
+    else{
+      if(inst.getOpcode() == Instruction::SDiv){
+        if(ConstantInt *C = dyn_cast<ConstantInt>(op1)){
+          if(C->getValue().isPowerOf2()){
+            auto numShift = C->getValue().logBase2();
+            Constant *C1 = ConstantInt::get(C->getType(), numShift);
+
+            Instruction *shiftInst = BinaryOperator::CreateAShr(op2, C1);
+            shiftInst->insertAfter(&inst);
+            inst.replaceAllUsesWith(shiftInst);
+
+            outs() << "DIV ";
+            print_strength_reduction(inst, shiftInst);
+          }
+        }
+        else{
+          if(ConstantInt *C = dyn_cast<ConstantInt>(op2)){
+            if(C->getValue().isPowerOf2()){
+              
+              auto numShift = C->getValue().logBase2();
+              Constant *C1 = ConstantInt::get(C->getType(), numShift);
+
+              Instruction *shiftInst = BinaryOperator::CreateAShr(op1, C1);
+              shiftInst->insertAfter(&inst);
+              inst.replaceAllUsesWith(shiftInst);
+
+              outs() << "DIV ";
+              print_strength_reduction(inst, shiftInst);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 bool runOnBasicBlock(BasicBlock &B) {  
   //Print all instruction
   /* outs() << "--------------------------------\n\n";
@@ -103,6 +223,7 @@ bool runOnBasicBlock(BasicBlock &B) {
   outs() << "\n--------------------------------\n\n\n"; */
 
   AlgebricIdentity(B);
+  StrengthReduction(B);
 
   // Preleviamo le prime due istruzioni del BB
   Instruction &Inst1st = *B.begin(), &Inst2nd = *(++B.begin());
@@ -110,7 +231,7 @@ bool runOnBasicBlock(BasicBlock &B) {
   // L'indirizzo della prima istruzione deve essere uguale a quello del 
   // primo operando della seconda istruzione (per costruzione dell'esempio)
   assert(&Inst1st == Inst2nd.getOperand(0));
-
+  outs() << "\n\n---------------------------------------\n\n";
   // Stampa la prima istruzione
   outs() << "PRIMA ISTRUZIONE: " << Inst1st << "\n";
   // Stampa la prima istruzione come operando
@@ -130,7 +251,8 @@ bool runOnBasicBlock(BasicBlock &B) {
     }
     if (ConstantInt *C = dyn_cast<ConstantInt>(Operand)) {
       outs() << "\t" << *C << ": SONO UNA COSTANTE INTERA DI VALORE " << C->getValue()
-              << "\n";
+              << "\n" ;
+      C->getType()->print(outs(), false, true);
     }
   }
 
@@ -176,4 +298,3 @@ PreservedAnalyses TestPass::run(Module &M, ModuleAnalysisManager &AM) {
   
   return PreservedAnalyses::all();
 }
-
